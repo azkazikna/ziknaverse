bb86fb80463014c9afaa369cae75bec0f4e19d0d
Azkazikna Ageung Laksana <azkazikna.aal@gmail.com>
2023-08-01 11:38:33 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*{html,js}"],
+  theme: {
+    fontFamily: {
+      'cerapro': ["Cera Pro", 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'hero-gradient': "url('/dist/images/header_bg.png')",
+        'all-content': "url('/dist/images/allcontent.png')",
+        'feature': "url('/dist/images/feature_bg.png')",
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s linear infinite',
+      },
+      colors: {
+        'primary': '#3070D4',
+        'secondary': '#F4F4F8'
+      }
+    },
+  },
+  plugins: [],
+}
+
